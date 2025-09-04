b12c86e4d623d145fd74f3d53470752ae17560f3
Ygor <ygorvieirafernades@gmail.com>
2025-09-04 19:51:10 -0300

    Finalização

--- Lista_Exercício1/Atividade1.php
+++ Lista_Exercício1/Atividade1.php
@@ -21,10 +21,12 @@
 </form>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
+<center>
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo "Resultado: " . ($_POST['num1'] + $_POST['num2']);
 }
 ?>
+</center>
 </body>
 </html>
