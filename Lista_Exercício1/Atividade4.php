291d31a9affcd9cc1e2fc0dc60e18c4e058284b1
Ygor <ygorvieirafernades@gmail.com>
2025-09-04 20:17:58 -0300

    Finalização Exercício 4

--- Lista_Exercício1/Atividade4.php
+++ Lista_Exercício1/Atividade4.php
@@ -1,19 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex4 - Divisão</title></head>
+<head><meta charset="UTF-8"><title>4. Divisão</title></head>
 <body>
-<h2>4. Divisão</h2>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>4. Divisão</h1>
 <form method="post">
-    <input type="number" name="n1" required> ÷
-    <input type="number" name="n2" required>
-    <button type="submit">Calcular</button>
+<div class="row inline-row mb-3"><div class="col-md-5">
+              <label for="num1" class="form-label">Digite um número: </label>
+              <input type="number" id="num1" name="num1" class="form-control" required="">
+            </div>
+            <div class="col-md-2 d-flex align-items-center justify-content-center">
+            <span style="font-size: 1.5rem;">/</span>
+            </div>
+            <div class="col-md-5">
+              <label for="num2" class="form-label">Digite um número: </label>
+              <input type="number" id="num2" name="num2" class="form-control" required="">
+            </div></div>
+            <center>
+<button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
-if ($_POST) {
-    if ($_POST['n2'] == 0) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['num2'] == 0) {
         echo "Erro: divisão por zero não é permitida.";
     } else {
-        echo "Resultado: " . ($_POST['n1'] / $_POST['n2']);
+        echo "Resultado: " . ($_POST['num1'] / $_POST['num2']);
     }
 }
 ?>
