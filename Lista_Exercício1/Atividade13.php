5ffee6c25d7ca28d4804c9b8fbf50382992ed3d3
Ygor <ygorvieirafernades@gmail.com>
2025-09-04 21:33:52 -0300

    Finalização exercício 13

--- Lista_Exercício1/Atividade13.php
+++ Lista_Exercício1/Atividade13.php
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex13 - Metros para cm</title></head>
-<body>
-<h2>13. Metros para centímetros</h2>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>13. Metros para Centímetros</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>13. Metros para Centímetros</h1>
 <form method="post">
-    <input type="number" name="m" step="0.01" required> m
-    <button type="submit">Converter</button>
+<div class="mb-3">
+              <label for="metros" class="form-label">Insira a quantidade de metros (m):</label>
+              <input type="number" id="metros" name="metros" class="form-control" required="">
+            </div>
+            <center>
+<button type="submit" class="btn btn-primary">Converter</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
 if ($_POST) {
-    echo "Centímetros: " . ($_POST['m'] * 100) . " cm";
+    echo "Centímetros: " . ($_POST['metros'] * 100) . " cm";
 }
 ?>
 </body>
