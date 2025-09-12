353f3ccf4711f88d7b1121dbf48b00440d79823a
Ygor <ygorvieirafernades@gmail.com>
2025-09-12 20:05:00 -0300

    Finalização exercício 20

--- Lista_Exercício1/Atividade20.php
+++ Lista_Exercício1/Atividade20.php
@@ -1,15 +1,30 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex20 - Velocidade Média</title></head>
-<body>
-<h2>20. Velocidade média</h2>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>20. Velocidade média</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
 <form method="post">
-    Distância (km): <input type="number" name="dist" step="0.01" required>
-    Tempo (h): <input type="number" name="tempo" step="0.01" required>
-    <button type="submit">Calcular</button>
+<div class="row inline-row mb-3"><div class="col-md-6">
+              <label for="dist" class="form-label">Distância (km):</label>
+              <input type="number" id="dist" name="dist" class="form-control" required="">
+            </div><div class="col-md-6">
+              <label for="tempo" class="form-label">Tempo (h):</label>
+              <input type="number" id="tempo" name="tempo" class="form-control" required="">
+            </div></div>
+            <center>
+<button type="submit" class="btn btn-primary">Calcular</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
     if ($_POST['tempo'] == 0) {
         echo "Erro: tempo não pode ser zero.";
     } else {
