d732f651ad7612c826e41b708f75375327706007
Ygor <ygorvieirafernades@gmail.com>
2025-09-12 19:58:30 -0300

    Finalização exercício 19

--- Lista_Exercício1/Atividade19.php
+++ Lista_Exercício1/Atividade19.php
@@ -1,14 +1,29 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex19 - Dias para H/M/S</title></head>
-<body>
-<h2>19. Converter dias em horas, minutos e segundos</h2>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>19. Converter dias em horas, minutos e segundos</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
 <form method="post">
-    Dias: <input type="number" name="dias" required>
-    <button type="submit">Converter</button>
+<div class="row inline-row mb-3">
+        <div class="col-md-12">
+              <label for="dias" class="form-label">Dias: </label>
+              <input type="number" id="dias" name="dias" class="form-control" required="" step="0.01">
+        </div>
+</div>
+<center>
+<button type="submit" class="btn btn-primary">Converter</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
     $dias = $_POST['dias'];
     $horas = $dias * 24;
     $minutos = $horas * 60;
