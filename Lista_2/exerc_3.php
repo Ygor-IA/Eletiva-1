78a950ec1434dbe42f830645a840400925cd5fe9
Ygor <ygorvieirafernades@gmail.com>
2025-09-18 20:00:44 -0300

    Consertando Exerc_3

--- Lista_2/exerc_3.php
+++ Lista_2/exerc_3.php
@@ -1,15 +1,28 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex3</title></head>
-<body>
-<h2>3. Ordem crescente</h2>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>3. Ordem Crescente</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
 <form method="post">
-    A: <input type="number" name="a" required>
-    B: <input type="number" name="b" required>
-    <button type="submit">Verificar</button>
+<div class="row inline-row mb-3"><div class="col-md-6">
+              <label for="a" class="form-label">Informe o Valor de A:</label>
+              <input type="number" id="a" name="a" class="form-control" required="">
+            </div><div class="col-md-6">
+              <label for="b" class="form-label">Informe o Valor de B:</label>
+              <input type="number" id="b" name="b" class="form-control" required="">
+            </div></div> <center>
+<button type="submit" class="btn btn-primary">Calcular</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $a = $_POST['a']; $b = $_POST['b'];
     if ($a == $b) {
         echo "Números iguais: $a";
