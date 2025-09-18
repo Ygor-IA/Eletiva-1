031695a38fdc63ac0ebea6db4936d09c1afe0e11
Ygor <ygorvieirafernades@gmail.com>
2025-09-18 20:05:52 -0300

    Consertando exerc_4

--- Lista_2/exerc_4.php
+++ Lista_2/exerc_4.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex4</title></head>
-<body>
-<h2>4. Preço com desconto se > 100</h2>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>4. Preço com desconto se > 100</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
 <form method="post">
-    Valor do produto: <input type="number" name="valor" step="0.01" required>
-    <button type="submit">Calcular</button>
+<div class="row inline-row mb-3"><div class="col-md-12">
+              <label for="valor" class="form-label">Valor do Produto: </label>
+              <input type="number" id="valor" name="valor" class="form-control" required="">
+            </div></div>
+            <center>
+<button type="submit" class="btn btn-primary">Calcular</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $valor = $_POST['valor'];
     if ($valor > 100) {
         $valor -= $valor * 0.15;
