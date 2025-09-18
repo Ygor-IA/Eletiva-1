634f2d46151f8c390e46fbbb1109dc3cdeeb1d3b
Ygor <ygorvieirafernades@gmail.com>
2025-09-18 19:56:45 -0300

    Consertando exerc_2.php

--- Lista_2/exerc_1.php
+++ Lista_2/exerc_1.php
@@ -3,12 +3,11 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title></title>
+<title>1. Menor Valor e Posição</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1></h1>
 <form method="post">
     <?php for ($i=1; $i<=7; $i++): ?>
 <div class="row inline-row mb-3"><div class="col-md-12">
