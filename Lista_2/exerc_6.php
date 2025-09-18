5286df1649cf006bb3629c04866320019e9305f8
Ygor <ygorvieirafernades@gmail.com>
2025-09-18 20:11:36 -0300

    Conserto exerc_6

--- Lista_2/exerc_6.php
+++ Lista_2/exerc_6.php
@@ -1,14 +1,26 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex6</title></head>
-<body>
-<h2>6. Imprimir de 1 até N (for)</h2>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>6. Imprimir de 1 até N</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
 <form method="post">
-    Número: <input type="number" name="n" required>
-    <button type="submit">Imprimir</button>
+<div class="row inline-row mb-3"><div class="col-md-12">
+              <label for="n" class="form-label">Informe um número:</label>
+              <input type="number" id="n" name="n" class="form-control" required="">
+            </div></div>
+            <center>
+<button type="submit" class="btn btn-primary">Imprimir</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $n = $_POST['n'];
     for ($i=1; $i<=$n; $i++) {
         echo $i . " ";
