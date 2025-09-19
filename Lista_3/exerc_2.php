629fafff994dfe8c85b687343d49e237dd5d04ad
Ygor <ygorvieirafernades@gmail.com>
2025-09-19 19:40:57 -0300

    Consertando Exerc_2

--- Lista_3/exerc_2.php
+++ Lista_3/exerc_2.php
@@ -1,12 +1,24 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex2</title></head>
-<body>
-<h2>2. Maiúsculo e minúsculo</h2>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>2. Maiúsculas e Minúsculas</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
 <form method="post">
-    Palavra: <input type="text" name="palavra" required>
-    <button type="submit">Transformar</button>
+<div class="row inline-row mb-3"><div class="col-md-12">
+              <label for="palavra" class="form-label">Digite uma palavra:</label>
+              <input type="text" id="palavra" name="palavra" class="form-control" required="">
+            </div></div>
+            <center>
+<button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
 function transformarTexto($texto) {
     // funções internas: strtoupper() e strtolower()
@@ -14,7 +26,7 @@ function transformarTexto($texto) {
            "Minúsculo: " . strtolower($texto);
 }
 
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo transformarTexto($_POST['palavra']);
 }
 ?>
