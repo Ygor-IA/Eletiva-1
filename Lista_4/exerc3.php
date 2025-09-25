951c67eb380020afde0800577a906ecdb966ea38
Ygor <ygorvieirafernades@gmail.com>
2025-09-25 20:11:12 -0300

    FInalizado Exercício 3

--- Lista_4/exerc3.php
+++ Lista_4/exerc3.php
@@ -6,19 +6,31 @@
 <form method="post">
     <?php for ($i=1; $i<=5; $i++): ?>
         <h3>Produto <?= $i ?></h3>
-        Código: <input type="text" name="codigo[]" required><br>
-        Nome: <input type="text" name="nome[]" required><br>
-        Preço: <input type="number" step="0.01" name="preco[]" required><br><br>
+        <div class="row inline-row mb-3"><div class="col-md-4">
+              <label for="codigo[]" class="form-label">Código:</label>
+              <input type="text" id="codigo[]" name="codigo[]" class="form-control" required="">
+            </div><div class="col-md-4">
+              <label for="nome[]" class="form-label">Informe seu nome:</label>
+              <input type="text" id="nome[]" name="nome[]" class="form-control" required="">
+            </div><div class="col-md-4">
+              <label for="preco[]" class="form-label">Informe o preço:</label>
+              <input type="number" id="preco[]" name="preco[]" class="form-control" required="">
+            </div></div>
     <?php endfor; ?>
-    <button type="submit">Salvar</button>
+    <div class="d-flex justify-content-center">
+    <button type="submit" class="btn btn-primary">Salvar</button>
+    </div>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
 if ($_POST) {
     $produtos = [];
     for ($i=0; $i<5; $i++) {
         $preco = $_POST['preco'][$i];
         if ($preco > 100) {
-            $preco *= 0.9; // desconto 10%
+            $preco *= 0.9; 
         }
         $produtos[$_POST['codigo'][$i]] = [
             "nome" => $_POST['nome'][$i],
@@ -26,7 +38,7 @@ if ($_POST) {
         ];
     }
 
-    // ordena pelo nome
+    
     uasort($produtos, function($a, $b) {
         return strcmp($a['nome'], $b['nome']);
     });
---
+++ ProjetoProfessora/conexao.php
@@ -0,0 +1,12 @@
+<?php
+    
+    $dominio = "mysql:host=localhost;dbname=projetophp";
+    $usuario = "root";
+    $senha = "";
+
+    try{
+
+
+    } catch (){
+        
+    }
