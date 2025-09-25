96396b7186851db262eaba2e7549397ac537c18f
Ygor <ygorvieirafernades@gmail.com>
2025-09-25 19:27:07 -0300

    Finalização Exercício 2

--- Lista_4/exerc1.php
+++ Lista_4/exerc1.php
@@ -1,18 +1,33 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head><meta charset="UTF-8"><title>Ex1 - Contatos</title></head>
-<body>
-<h2>1. Lista de contatos</h2>
+</body>
+</html>
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>1. Lista de Contatos</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
 <form method="post">
     <?php for ($i=1; $i<=5; $i++): ?>
         <h3>Contato <?= $i ?></h3>
-        Nome: <input type="text" name="nome[]" required>
-        Telefone: <input type="text" name="tel[]" required><br><br>
+        <div class="row inline-row mb-3"><div class="col-md-6">
+              <label for="nome[]" class="form-label">Informe seu nome:</label>
+              <input type="text" id="nome[]" name="nome[]" class="form-control" required="">
+            </div><div class="col-md-6">
+              <label for="tel[]" class="form-label">Informe seu telefone:</label>
+              <input type="number" id="tel[]" name="tel[]" class="form-control" required="">
+            </div></div>
     <?php endfor; ?>
-    <button type="submit">Salvar</button>
+    <button type="submit" class="btn btn-primary">Salvar</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+<center>
 <?php
-if ($_POST) {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nomes = $_POST['nome'];
     $tels = $_POST['tel'];
     $contatos = [];
@@ -28,7 +43,7 @@ if ($_POST) {
         }
     }
 
-    ksort($contatos); // ordena por nome
+    ksort($contatos);
     echo "<h3>Lista de contatos:</h3>";
     foreach ($contatos as $nome => $tel) {
         echo "$nome → $tel<br>";
